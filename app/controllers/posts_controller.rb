77013d1ad64c1804d0a44ea289c43f2df36fef01
ranmaru-seki <ranma19.sar@gmail.com>
2020-07-22 17:38:25 +0900

    non-follow-text-update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,23 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:show, :destroy]
+
   def index
-    @user  = User.find_by(id: current_user.id)
+    @user = User.find_by(id: current_user.id)
     @users = @user.followings
+    @like = Like.new
     @posts = []
     @users.each do |user|
       posts = Post.where(user_id: user.id).order(created_at: :desc)
-      #取得したユーザーの投稿一覧を@postsに格納
       @posts.concat(posts)
     end
     myposts = Post.where(user_id: current_user.id)
     @posts.concat(myposts)
-    #@postsを新しい順に並べたい
     @posts.sort_by!{|post| post.created_at}.reverse!
     if @posts.nil?
       flash[:notice]="まだ投稿がありません…"
       redirect_to("/")
     end
-
-    @like = Like.new
   end
 
   def new
@@ -37,7 +36,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
     @comment = Comment.new
     @comments = @post.comments.includes(:user)
     @like = Like.find_by(post_id: params[:id])
@@ -45,10 +43,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    flash[:notice] = "投稿を削除しました"
-    redirect_to root_path
+    if current_user.id == @post.user_id
+      @post.destroy
+      flash[:notice] = "投稿を削除しました"
+      redirect_to root_path
+    else
+      flash[:alert] = "その操作はできません"
+      redirect_to root_path
+    end
   end
 
   def search
@@ -60,10 +62,13 @@ class PostsController < ApplicationController
     end
   end
 
-
   private
   def post_params
     params.require(:post).permit(:contents, :image).merge(user_id:current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
